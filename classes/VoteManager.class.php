0cf38c0482dd634502430e8ddf5b10dc819bfbc8
Guillaume BREHIER <brehier.guillaume@orange.fr>
2019-11-30 22:58:06 +0100

    Vote Manager terminée

--- classes/VoteManager.class.php
+++ classes/VoteManager.class.php
@@ -1,7 +1,19 @@
-<?php class VilleManager{
+<?php class VoteManager{
 
     public function __construct($db){
       $this->db=$db;
+    }
+
+    public function voterCitation($citNum,$perNum,$votValeur){
+      $requete = $this->db->prepare(
+        'INSERT INTO vote (cit_num,per_num,vot_valeur) VALUES (:cit_num,:per_num,:vot_valeur);');
+        $requete->bindValue(':cit_num',$citNum);
+        $requete->bindValue(':per_num',$perNum);
+        $requete->bindValue(':vot_valeur',$votValeur);
+
+        $retour=$requete->execute();
+        return $retour;
+
     }
 
 }
