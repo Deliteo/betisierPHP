ecd85ed4460afd72fc918ff62ad92f52b2e1a422
Guillaume BREHIER <brehier.guillaume@orange.fr>
2019-11-30 22:57:45 +0100

    Classe Vote terminée

--- classes/Vote.class.php
+++ classes/Vote.class.php
@@ -11,6 +11,42 @@ class Vote {
       $this->affecte($valeurs);
   }
 
+  public function affecte($donnees){
+    foreach ($donnees as $attribut => $valeurs){
+      switch($attribut){
+
+        case 'cit_num' : $this->setNumCit($valeurs);
+        break;
+
+        case 'per_num' : $this->setNumPer($valeurs);
+        break;
+
+        case 'vot_valeur' : $this->setVotValeur($valeurs);
+        break;
+      }
+    }
+  }
+
+  public function setNumCit($citnum){
+    $this->citNum=$citnum;
+  }
+  public function setNumPer($pernum){
+    $this->perNum=$pernum;
+  }
+  public function setVotValeur($valvot){
+    $this->votValeur=$valvot;
+  }
+
+  public function getNumCit(){
+    return $this->citNum;
+  }
+  public function getNumPer(){
+    return $this->perNum;
+  }
+  public function getVotValeur(){
+    return $this->votValeur;
+  }
+
 }
 
 ?>
